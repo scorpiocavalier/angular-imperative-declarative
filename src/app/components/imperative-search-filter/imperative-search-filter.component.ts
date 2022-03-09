ad4cc6298b61b91c3b05b23354acfc7af8250170
Arturo Chen <art.can.fly@gmail.com>
2022-03-08 21:48:39 -0500

    formControl approach for searching

--- src/app/components/imperative-search-filter/imperative-search-filter.component.ts
+++ src/app/components/imperative-search-filter/imperative-search-filter.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 import { Client } from 'src/app/models/client';
 import { ClientService } from 'src/app/services/client.service';
 
@@ -7,26 +9,43 @@ import { ClientService } from 'src/app/services/client.service';
   templateUrl: './imperative-search-filter.component.html',
   styleUrls: ['./imperative-search-filter.component.css'],
 })
-export class ImperativeSearchFilterComponent implements OnInit {
-  filter: string = '';
-  clients: Client[] = [];
-  filteredClients: Client[] = [];
+export class ImperativeSearchFilterComponent implements OnInit, OnDestroy {
+  searchField: FormControl = new FormControl('');
+  clients: Client[];
+  filteredClients: Client[];
+
+  private destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private clientService: ClientService) {}
 
   ngOnInit(): void {
-    this.clientService.getClients().subscribe((clients: Client[]) => {
-      this.clients = clients;
-      this.filteredClients = clients;
-    });
+    this.clientService
+      .getClients()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((clients: Client[]) => {
+        this.clients = clients;
+        this.filteredClients = clients;
+      });
+
+    this.searchField.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((searchTerm: string) => {
+        this.filteredClients = this.clients.filter(
+          ({ firstName, lastName }) => {
+            const fullName = [firstName, lastName].join(' ');
+            return (
+              searchTerm === '' ||
+              [fullName, firstName, lastName].some((name) =>
+                name.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+            );
+          }
+        );
+      });
   }
 
-  filterList(): void {
-    this.filteredClients = this.clients.filter(({ firstName, lastName }) => {
-      const fullName = [firstName, lastName].join(' ');
-      return [firstName, lastName, fullName].some((name: string) =>
-        name.toLowerCase().includes(this.filter.toLowerCase())
-      );
-    });
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 }
